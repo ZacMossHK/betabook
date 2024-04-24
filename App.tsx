33f7e3f07bf662491c3e038c4b61742193d3c70a
Zac <zac@zacmoss.co.uk>
2024-04-24 12:47:48 +0100

    feat: created popluatedNodes function for development

--- App.tsx
+++ App.tsx
@@ -15,49 +15,14 @@ import { getMatrix } from "./src/helpers/matrixTransformers/utils";
 import ImageContainer from "./src/components/ImageContainer";
 import SvgContainer from "./src/components/SvgContainer";
 import { Canvas, Line } from "@shopify/react-native-skia";
-const testNodes = [
-  { x: 0, y: 0 },
-  { x: 298.0800895690918, y: 442.5572052001953 },
-  { x: 312.2549133300781, y: 287.9411926269531 },
-  { x: 231.86276245117188, y: 362.45098876953125 },
-  { x: 192.64706420898438, y: 547.1568603515625 },
-  { x: 249.9019775390625, y: 280.0980529785156 },
-  { x: 134.21568298339844, y: 444.0196228027344 },
-  { x: 272.2549133300781, y: 573.0392456054688 },
-  { x: 224.41177368164062, y: 452.6470642089844 },
-  { x: 103.62745666503906, y: 616.5686645507812 },
-  { x: 174.833251953125, y: 254.36387634277344 },
-  { x: 318.5294189453125, y: 387.9411926269531 },
-  { x: 180.09803771972656, y: 242.45098876953125 },
-  { x: 277.3529357910156, y: 188.72549438476562 },
-  { x: 243.72695922851562, y: 696.9608154296875 },
-  { x: 109.50981140136719, y: 560.8823852539062 },
-  { x: 134.21568298339844, y: 444.0196228027344 },
-  { x: 272.2549133300781, y: 573.0392456054688 },
-  { x: 224.41177368164062, y: 452.6470642089844 },
-  { x: 103.62745666503906, y: 616.5686645507812 },
-  { x: 174.833251953125, y: 254.36387634277344 },
-  { x: 318.5294189453125, y: 387.9411926269531 },
-  { x: 180.09803771972656, y: 242.45098876953125 },
-  { x: 277.3529357910156, y: 188.72549438476562 },
-  { x: 243.72695922851562, y: 696.9608154296875 },
-  { x: 109.50981140136719, y: 560.8823852539062 },
-  { x: 298.0800895690918, y: 442.5572052001953 },
-  { x: 312.2549133300781, y: 287.9411926269531 },
-  { x: 231.86276245117188, y: 362.45098876953125 },
-  { x: 192.64706420898438, y: 547.1568603515625 },
-  { x: 249.9019775390625, y: 280.0980529785156 },
-  { x: 134.21568298339844, y: 444.0196228027344 },
-  { x: 272.2549133300781, y: 573.0392456054688 },
-  { x: 224.41177368164062, y: 452.6470642089844 },
-  { x: 103.62745666503906, y: 616.5686645507812 },
-  { x: 174.833251953125, y: 254.36387634277344 },
-  { x: 318.5294189453125, y: 387.9411926269531 },
-  { x: 180.09803771972656, y: 242.45098876953125 },
-  { x: 277.3529357910156, y: 188.72549438476562 },
-  { x: 243.72695922851562, y: 696.9608154296875 },
-  { x: 109.50981140136719, y: 560.8823852539062 },
-];
+import populateNodes from "./devData/populateNodes";
+
+const populatedNodes = populateNodes(
+  process.env.EXPO_PUBLIC_NODES_NUM
+    ? parseInt(process.env.EXPO_PUBLIC_NODES_NUM)
+    : 0
+);
+
 const ImageViewer = () => {
   const ref = useAnimatedRef();
 
@@ -73,11 +38,10 @@ const ImageViewer = () => {
   const isSelectingNode = useSharedValue(false);
   const isTranslatingNode = useSharedValue(false);
 
-  /* The first element should never be changed or read. 
+  /* The first element in the nodes array should never be changed or read. 
   There must be already be a first element in the array as the first node in the array blinks in and out if it's deleted.
   I don't know why it does this. */
-  // const [nodes, setNodes] = useState<Nodes>([{ x: 0, y: 0 }]);
-  const [nodes, setNodes] = useState<Nodes>(testNodes);
+  const [nodes, setNodes] = useState<Nodes>(populatedNodes);
 
   useEffect(() => {
     selectedNodePosition.value = null;
---
+++ devData/populateNodes.ts
@@ -0,0 +1,28 @@
+import { Nodes } from "../src/components/ImageViewer/index.types";
+
+const hasOverlap = (existingPoints: Nodes, newX: number, newY: number) => {
+  for (let i = 0; i < existingPoints.length; i++) {
+    const existingPoint = existingPoints[i];
+    const distance = Math.sqrt(
+      (existingPoint.x - newX) ** 2 + (existingPoint.y - newY) ** 2
+    );
+    const minDistance = 50;
+    if (distance < minDistance) return true;
+  }
+  return false;
+};
+
+const populateNodes = (nodeQuantity: number): Nodes => {
+  const nodes = [{ x: 0, y: 0 }];
+  for (let i = 0; i < nodeQuantity; i++) {
+    let x, y;
+    do {
+      x = Math.random() * (403 - 10) + 10;
+      y = Math.random() * (713 - 188) + 188;
+    } while (hasOverlap(nodes, x, y));
+    nodes.push({ x, y });
+  }
+  return nodes;
+};
+
+export default populateNodes;
