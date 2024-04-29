6a49a67493951bbceaa918b6f53323406f0cea0f
Zac <zac@zacmoss.co.uk>
2024-04-29 16:35:10 +0100

    refactor: generateTransform is used in useAnimatedStyle

--- src/components/MovementNodeLine/index.tsx
+++ src/components/MovementNodeLine/index.tsx
@@ -18,11 +18,12 @@ const getNodeXYWithOffset = (node: Coordinates) => {
   return [x, y].map((n) => n + NODE_SIZE_OFFSET);
 };
 
-const generateStaticTransform = (
+const generateTransform = (
   currentNode: Coordinates,
   nextNode: Coordinates,
   ratioDiff: number
 ) => {
+  "worklet";
   const [x1, y1] = getNodeXYWithOffset(currentNode);
   const [x2, y2] = getNodeXYWithOffset(nextNode);
   return [
@@ -103,44 +104,21 @@ const MovementNodeLine = ({
           width: 1,
           flex: 1,
           position: "absolute",
-          transform: generateStaticTransform(currentNode, nextNode, ratioDiff),
+          transform: generateTransform(currentNode, nextNode, ratioDiff),
         },
-        useAnimatedStyle(() => {
-          if (
-            [nodeIndex, nodeIndex + 1].some(
-              (index) => index === adjustedPositionNodes.value.length
-            )
-          ) {
-            return {};
-          }
-          const [x1, y1] = getNodeXYWithOffset(
-            adjustedPositionNodes.value[nodeIndex]
-          );
-          const [x2, y2] = getNodeXYWithOffset(
-            adjustedPositionNodes.value[nodeIndex + 1]
-          );
-          return {
-            transform: [
-              { translateX: x1 },
-              { translateY: y1 },
-              // formulas for width and rotation pilfered from https://jsfiddle.net/tmjLu8sb/4/
-              {
-                rotate:
-                  // needs to add 180 degrees because the maths puts the angle in the exact opposite direction
-                  (
-                    Math.atan2(y1 - y2, x1 - x2) * (180 / Math.PI) +
-                    180
-                  ).toString() + "deg",
-              },
-              {
-                scaleX:
-                  Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)) *
-                  // ratioDiff fixes pixel density issue from scaling - https://github.com/facebook/react-native/issues/41403#issuecomment-1805532160
-                  ratioDiff,
-              },
-            ],
-          };
-        }),
+        useAnimatedStyle(() =>
+          [nodeIndex, nodeIndex + 1].some(
+            (index) => index === adjustedPositionNodes.value.length
+          )
+            ? {}
+            : {
+                transform: generateTransform(
+                  adjustedPositionNodes.value[nodeIndex],
+                  adjustedPositionNodes.value[nodeIndex + 1],
+                  ratioDiff
+                ),
+              }
+        ),
       ]}
     />
   );
