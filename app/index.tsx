f505250708ae439d96be505bc1ac043f55076b2c
Zac <zac@zacmoss.co.uk>
2024-05-20 15:12:17 +0100

    refactor: expanded Menu component into index

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,15 @@
-import { SafeAreaView, Text } from "react-native";
+import { Button, SafeAreaView, Text, View } from "react-native";
 import { ImageProps, Nodes } from "../src/components/ImageViewer/index.types";
-import Menu from "../src/components/Menu";
-import { Stack } from "expo-router";
+import { Stack, useRouter } from "expo-router";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { useClimb } from "../src/providers/ClimbProvider";
+import { useEffect, useState } from "react";
+import devCurrentFile from "../devData/devCurrentfile";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import * as ImagePicker from "expo-image-picker";
+import * as FileSystem from "expo-file-system";
+import { randomUUID } from "expo-crypto";
+import { IMAGE_DIR } from "../src/components/Menu/index.constants";
 
 export interface File {
   fileId: string;
@@ -14,29 +22,128 @@ export type SetCurrentFileState = React.Dispatch<
   React.SetStateAction<File | null>
 >;
 
-const App = () => (
-  <SafeAreaView
-    style={{
-      flex: 1,
-      backgroundColor: "#F55536",
-      alignItems: "center",
-      justifyContent: "center",
-      padding: 0,
-    }}
-  >
-    <Stack.Screen options={{ headerShown: false }} />
-    <Text
+const Menu = () => {
+  const router = useRouter();
+  const { climb, setClimb } = useClimb();
+
+  const [savedFiles, setSavedFiles] = useState<File[]>([]);
+  const [isRequestingDeletingFiles, setIsRequestingDeletingFiles] =
+    useState(false);
+
+  const loadFiles = async () => {
+    const files = [];
+    // file for development only
+    if (process.env.EXPO_PUBLIC_NODES_NUM || process.env.EXPO_PUBLIC_DEV_IMG)
+      files.push(devCurrentFile());
+    for (const fileId of await AsyncStorage.getAllKeys()) {
+      const file = await AsyncStorage.getItem(fileId);
+      if (file) files.push(JSON.parse(file));
+    }
+    await setSavedFiles(files);
+  };
+
+  useEffect(() => {
+    if (climb) return;
+    loadFiles();
+  }, [climb]);
+
+  const loadFile = async (file: File) => {
+    await setClimb(file);
+    router.navigate("imageViewer");
+  };
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    const result = await ImagePicker.launchImageLibraryAsync({
+      quality: 1,
+    });
+    if (result.canceled) return;
+    const { uri, height, width } = result.assets[0];
+    await setClimb({
+      fileId: randomUUID(),
+      fileName: null,
+      imageProps: { uri, height, width },
+      nodes: [],
+    });
+
+    router.navigate("imageViewer");
+  };
+
+  const deleteAllFiles = async () => {
+    await AsyncStorage.multiRemove(await AsyncStorage.getAllKeys());
+    await FileSystem.deleteAsync(IMAGE_DIR);
+    await setSavedFiles([]);
+    await setIsRequestingDeletingFiles(false);
+  };
+  return (
+    <SafeAreaView
       style={{
-        color: "white",
-        fontSize: 69,
-        marginBottom: 29,
-        fontFamily: "InriaSerif_400Regular",
+        flex: 1,
+        backgroundColor: "#F55536",
+        alignItems: "center",
+        justifyContent: "center",
+        padding: 0,
       }}
     >
-      Betabook
-    </Text>
-    <Menu />
-  </SafeAreaView>
-);
+      <Stack.Screen options={{ headerShown: false }} />
+      <Text
+        style={{
+          color: "white",
+          fontSize: 69,
+          marginBottom: 29,
+          fontFamily: "InriaSerif_400Regular",
+        }}
+      >
+        Betabook
+      </Text>
+      <View>
+        <TouchableOpacity
+          style={{ backgroundColor: "#D6EFFF", borderRadius: 15, padding: 9 }}
+          onPress={pickImage}
+        >
+          <Text
+            style={{
+              fontSize: 19,
+              fontFamily: "InriaSans_700Bold",
+              color: "#14281D",
+            }}
+          >
+            Create new
+          </Text>
+        </TouchableOpacity>
+        {savedFiles.length ? (
+          <>
+            {isRequestingDeletingFiles ? (
+              <>
+                <Button
+                  onPress={deleteAllFiles}
+                  title="Confirm file Deletion - cannot be undone!"
+                  color="red"
+                />
+                <Button
+                  title="Cancel"
+                  onPress={() => setIsRequestingDeletingFiles(false)}
+                />
+              </>
+            ) : (
+              <Button
+                onPress={() => setIsRequestingDeletingFiles(true)}
+                title="Delete all files"
+                color="red"
+              />
+            )}
+            {savedFiles.map((savedFile, index) => (
+              <Button
+                onPress={() => loadFile(savedFile)}
+                key={index}
+                title={savedFile.fileName || ""}
+              />
+            ))}
+          </>
+        ) : null}
+      </View>
+    </SafeAreaView>
+  );
+};
 
-export default App;
+export default Menu;
