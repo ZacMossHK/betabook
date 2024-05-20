c1723859d85c83882821c731f784cb697b97cba6
Zac <zac@zacmoss.co.uk>
2024-05-20 11:09:39 +0100

    chore: moved fontsLoaded to _layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -15,104 +15,122 @@ import { useEffect, useRef, useState } from "react";
 import IsEditingTitleProvider, {
   useIsEditingTitle,
 } from "../src/providers/EditingTitleProvider";
+import { InriaSerif_400Regular } from "@expo-google-fonts/inria-serif";
+import {
+  InriaSans_400Regular,
+  InriaSans_700Bold,
+} from "@expo-google-fonts/inria-sans";
+import { useFonts } from "expo-font";
 
-const RootLayout = () => (
-  <GestureHandlerRootView style={{ flex: 1 }}>
-    <IsEditingTitleProvider>
-      <ClimbProvider>
-        <Stack
-          screenOptions={{
-            headerStyle: { backgroundColor: "#F55536" },
+const RootLayout = () => {
+  const [fontsLoaded] = useFonts({
+    InriaSerif_400Regular,
+    InriaSans_400Regular,
+    InriaSans_700Bold,
+  });
 
-            headerRight: (props) => {
-              const { isEditingTitle, setIsEditingTitle } = useIsEditingTitle();
-              const { saveClimb } = useClimb();
-              if (!isEditingTitle) return;
-              return (
-                <TouchableOpacity
-                  style={{
-                    padding: 9,
-                    backgroundColor: "#D6EFFF",
-                    borderRadius: 15,
-                  }}
-                  onPress={() => {
-                    saveClimb();
-                    setIsEditingTitle(false);
-                  }}
-                >
-                  <Text
-                    style={{
-                      fontSize: 12,
-                      fontFamily: "InriaSans_700Bold",
-                      color: "#14281D",
-                    }}
-                  >
-                    OK
-                  </Text>
-                </TouchableOpacity>
-              );
-            },
-            headerTitle: (props) => {
-              const { isEditingTitle, setIsEditingTitle } = useIsEditingTitle();
-              const { climb, newClimbName, setNewClimbName, saveClimb } =
-                useClimb();
+  // TODO: replace with splash screen
+  if (!fontsLoaded) return null;
+  
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <IsEditingTitleProvider>
+        <ClimbProvider>
+          <Stack
+            screenOptions={{
+              headerStyle: { backgroundColor: "#F55536" },
 
-              if (isEditingTitle) {
+              headerRight: (props) => {
+                const { isEditingTitle, setIsEditingTitle } =
+                  useIsEditingTitle();
+                const { saveClimb } = useClimb();
+                if (!isEditingTitle) return;
                 return (
-                  <View
+                  <TouchableOpacity
                     style={{
-                      flex: 1,
-                      flexDirection: "row",
+                      padding: 9,
+                      backgroundColor: "#D6EFFF",
+                      borderRadius: 15,
+                    }}
+                    onPress={() => {
+                      saveClimb();
+                      setIsEditingTitle(false);
                     }}
                   >
-                    <TextInput
+                    <Text
                       style={{
-                        borderRadius: 15,
-                        paddingHorizontal: 9,
-                        backgroundColor: "white",
-                        height: 40,
-                        textAlign: "center",
-                        fontFamily: "InriaSans_400Regular",
+                        fontSize: 12,
+                        fontFamily: "InriaSans_700Bold",
                         color: "#14281D",
+                      }}
+                    >
+                      OK
+                    </Text>
+                  </TouchableOpacity>
+                );
+              },
+              headerTitle: (props) => {
+                const { isEditingTitle, setIsEditingTitle } =
+                  useIsEditingTitle();
+                const { climb, newClimbName, setNewClimbName, saveClimb } =
+                  useClimb();
+
+                if (isEditingTitle) {
+                  return (
+                    <View
+                      style={{
+                        flex: 1,
+                        flexDirection: "row",
+                      }}
+                    >
+                      <TextInput
+                        style={{
+                          borderRadius: 15,
+                          paddingHorizontal: 9,
+                          backgroundColor: "white",
+                          height: 40,
+                          textAlign: "center",
+                          fontFamily: "InriaSans_400Regular",
+                          color: "#14281D",
+                          fontSize: 22,
+                        }}
+                        placeholder={"Enter climb name..."}
+                        defaultValue={climb?.fileName || ""}
+                        onChangeText={setNewClimbName}
+                        onLayout={(e) => e.target.focus()}
+                        onBlur={() => {
+                          saveClimb();
+                          setIsEditingTitle(false);
+                        }}
+                      />
+                    </View>
+                  );
+                }
+
+                return (
+                  <Pressable
+                    style={{ flex: 1 }}
+                    onPress={() => {
+                      setIsEditingTitle(true);
+                    }}
+                  >
+                    <Text
+                      style={{
+                        fontFamily: "InriaSans_400Regular",
+                        color: "white",
                         fontSize: 22,
                       }}
-                      placeholder={"Enter climb name..."}
-                      defaultValue={climb?.fileName || ""}
-                      onChangeText={setNewClimbName}
-                      onLayout={(e) => e.target.focus()}
-                      onBlur={() => {
-                        saveClimb();
-                        setIsEditingTitle(false);
-                      }}
-                    />
-                  </View>
+                    >
+                      {climb?.fileName || ""}
+                    </Text>
+                  </Pressable>
                 );
-              }
-
-              return (
-                <Pressable
-                  style={{ flex: 1 }}
-                  onPress={() => {
-                    setIsEditingTitle(true);
-                  }}
-                >
-                  <Text
-                    style={{
-                      fontFamily: "InriaSans_400Regular",
-                      color: "white",
-                      fontSize: 22,
-                    }}
-                  >
-                    {climb?.fileName || ""}
-                  </Text>
-                </Pressable>
-              );
-            },
-          }}
-        />
-      </ClimbProvider>
-    </IsEditingTitleProvider>
-  </GestureHandlerRootView>
-);
-
+              },
+            }}
+          />
+        </ClimbProvider>
+      </IsEditingTitleProvider>
+    </GestureHandlerRootView>
+  );
+};
 export default RootLayout;
--- app/index.tsx
+++ app/index.tsx
@@ -2,14 +2,6 @@ import { SafeAreaView, Text } from "react-native";
 import { ImageProps, Nodes } from "../src/components/ImageViewer/index.types";
 import Menu from "../src/components/Menu";
 import { Stack } from "expo-router";
-import {
-  useFonts,
-  InriaSerif_400Regular,
-} from "@expo-google-fonts/inria-serif";
-import {
-  InriaSans_400Regular,
-  InriaSans_700Bold,
-} from "@expo-google-fonts/inria-sans";
 
 export interface File {
   fileId: string;
@@ -22,40 +14,29 @@ export type SetCurrentFileState = React.Dispatch<
   React.SetStateAction<File | null>
 >;
 
-const App = () => {
-  const [fontsLoaded] = useFonts({
-    InriaSerif_400Regular,
-    InriaSans_400Regular,
-    InriaSans_700Bold,
-  });
-
-  // TODO: replace with splash screen
-  if (!fontsLoaded) return null;
-
-  return (
-    <SafeAreaView
+const App = () => (
+  <SafeAreaView
+    style={{
+      flex: 1,
+      backgroundColor: "#F55536",
+      alignItems: "center",
+      justifyContent: "center",
+      padding: 0,
+    }}
+  >
+    <Stack.Screen options={{ headerShown: false }} />
+    <Text
       style={{
-        flex: 1,
-        backgroundColor: "#F55536",
-        alignItems: "center",
-        justifyContent: "center",
-        padding: 0,
+        color: "white",
+        fontSize: 69,
+        marginBottom: 29,
+        fontFamily: "InriaSerif_400Regular",
       }}
     >
-      <Stack.Screen options={{ headerShown: false }} />
-      <Text
-        style={{
-          color: "white",
-          fontSize: 69,
-          marginBottom: 29,
-          fontFamily: "InriaSerif_400Regular",
-        }}
-      >
-        Betabook
-      </Text>
-      <Menu />
-    </SafeAreaView>
-  );
-};
+      Betabook
+    </Text>
+    <Menu />
+  </SafeAreaView>
+);
 
 export default App;
